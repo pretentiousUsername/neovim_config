bd7567a62aabb1fa84f4199d78186d16c3031b58
Ian Mitchell <mitchell.ian.2001@gmail.com>
2026-03-01 14:55:32 -0500

    lsp config stuff

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -17,10 +17,51 @@ vim.lsp.config["lua_ls"] = {
 vim.lsp.config["pyright"] = {
     cmd = { "pyright-langserver", "--stdio" },
     filetypes = { "python" },
+    settings = {
+        pyright = {
+            disableOrganizedImports = true,
+        },
+        python = {
+            analysis = {
+                ignore = { "*" },
+            },
+        },
+    },
 }
+
+vim.lsp.config["ruff"] = {
+    cmd = { "ruff", "server" },
+    filetypes = { "python" },
+    init_options = {
+        settings = {
+            pycodestyle = {
+                maxLineLength = 100,
+            },
+            showSyntaxErrors = true,
+            logLevel = "debug",
+        },
+    },
+}
+
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup('lsp_attach_disable_ruff_hover', { clear = true }),
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client == nil then
+      return
+    end
+    if client.name == 'ruff' then
+      -- Disable hover in favor of Pyright
+      client.server_capabilities.hoverProvider = false
+    end
+  end,
+  desc = 'LSP: Disable hover capability from Ruff',
+})
 
 
 
 -- enable the LSPs
 -- vim.lsp.enable("lua_ls")
 vim.lsp.enable("pyright")
+vim.lsp.enable("ruff")
---
+++ lua/config/test.py
@@ -0,0 +1,5 @@
+# This is just for testing out LSPs and stuff---in this case, pyright and
+# ruff. I might just go back to pylsp is things get desparate enough.
+import asdf
+
+"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
